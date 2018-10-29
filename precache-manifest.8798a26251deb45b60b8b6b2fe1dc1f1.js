ca2289ac40b3d17e12094311ca397e0454ca92a9
BIRD-MAC <chalermporn@aspiron.co.th>
2018-10-29 12:26:30 +0700

    deploy

---
+++ precache-manifest.8798a26251deb45b60b8b6b2fe1dc1f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1eaa38473577995c90",
+    "url": "/js/about.8a391d75.js"
+  },
+  {
+    "revision": "2615cf47a00dcfb61614",
+    "url": "/css/app.ff7c4f7d.css"
+  },
+  {
+    "revision": "2615cf47a00dcfb61614",
+    "url": "/js/app.c97e8fd8.js"
+  },
+  {
+    "revision": "859eacc480f25650a49f",
+    "url": "/js/chunk-vendors.272e5479.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4eba6f9d7fc0695fba55492ed087ad71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
